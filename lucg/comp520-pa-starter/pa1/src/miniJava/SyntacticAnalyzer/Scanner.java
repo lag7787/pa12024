92be45a26fcc89331bcf334e81a0d45e9646363b
luc <garabrantluc@gmail.com>
2024-01-23 13:53:37 -0500

    added space sepeartor etc

--- lucg/comp520-pa-starter/pa1/src/miniJava/SyntacticAnalyzer/Scanner.java
+++ lucg/comp520-pa-starter/pa1/src/miniJava/SyntacticAnalyzer/Scanner.java
@@ -20,13 +20,12 @@ public class Scanner {
 	}
 	
 	public Token scan() {
+		skipSeparators();
 		// TODO: This function should check the current char to determine what the token could be.
 		
-		// TODO: Consider what happens if the current char is whitespace
-		
-		// TODO: Consider what happens if there is a comment (// or /* */)
-		
 		// TODO: What happens if there are no more tokens?
+		// how do we know if there are no more tokens? 
+			// eot?  return EOT token? 
 		
 		// TODO: Determine what the token is. For example, if it is a number
 		//  keep calling takeIt() until _currentChar is not a number. Then
@@ -42,21 +41,166 @@ public class Scanner {
 	private void skipIt() {
 		nextChar();
 	}
+
+	private void skipSeparators() {
+		// could check two characters??
+		switch (_currentChar) {
+		case ' ': case '\t': case '\r': case '\n':
+			skipIt();
+			skipSeparators();
+			break;
+		case '/':
+			// if next character isnt a forward slash or or * its not a comment, restore old byyes
+			_in.mark(1);
+			skipIt();
+			if (_currentChar == '*') {
+				skipIt();
+				char previousChar = _currentChar;
+				skipIt();
+				while (!(eot || (previousChar == '*' && _currentChar == '/'))) {
+					previousChar = _currentChar;
+					skipIt();
+				}
+				skipIt();
+			} else if (_currentChar == '/') {
+				skipIt();
+				while (!(eot || _currentChar == '\n' || _currentChar == '\r')) {
+					skipIt();
+				}
+				skipIt();
+			} else {
+				_currentChar = '/';
+				try {
+					_in.reset();
+				} catch (IOException e) {
+					scanError(e.toString());
+				}
+				
+				break;
+			}
+			skipSeparators();
+			break;
+		default:
+			break;
+		}
+	}	
 	
 	private void nextChar() {
 		try {
 			int c = _in.read();
-			_currentChar = (char)c;
+			if (c > 128) {
+				scanError("NonASCII input!");
+				// handling for non ASCII input. not sure if this is correct
+			}
 			
+			// result of (char) -1 ?
 			if (c == -1) {
 				eot = true;
 			}
-			
-			// TODO: What happens if c is not a regular ASCII character?
+
+			_currentChar = (char)c;
 			
 		} catch( IOException e ) {
 			scanError("I/O Exception!");
 		}
+	}
+
+	public TokenKind scanToken() {
+		
+		if (eot)
+			return(TokenKind.EOT); 
+
+		// scan Token
+		switch (currentChar) {
+		case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i':case 'j':
+		case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't':
+		case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+		case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':case 'J':
+		case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T':
+		case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+			takeIt();
+			while (Character.isLetter(currentChar) || 
+					Character.isDigit(currentChar) || 
+					currentChar == '_')
+				takeIt();
+			if (keywordMap.containsKey(this.currentSpelling.toString().toLowerCase())) {
+				return keywordMap.get(this.currentSpelling.toString().toLowerCase());
+			} else {
+				return TokenKind.IDENTIFIER;
+			}
+		//some characters require two character look ahead. 
+		//single = is not an operator, but a keyword 
+		case '+': case '-': case '*': case '/':
+		case '&': case '|': case '!':
+		case '>': case '<':
+			if (currentChar == '/') {
+				System.out.println();
+			}
+			char prevChar = currentChar;
+			takeIt();
+			//were trying to decide if we should take a second char. 
+			switch (prevChar) {
+			case '>': case '<': case '!':
+				if (currentChar == '=')
+					takeIt();
+					break;
+			case '&': case '|':
+				if (currentChar == prevChar)
+					takeIt();
+					break;
+			}
+			return(TokenKind.OPERATOR);
+		case '0': case '1': case '2': case '3': case '4': case '5':
+		case '6': case '7': case '8': case  '9':
+			takeIt();
+			while (Character.isDigit(currentChar)) takeIt();
+			return TokenKind.NUMBER;
+		case '=':
+			takeIt();
+			if (currentChar == '=') {
+				takeIt();
+				return (TokenKind.OPERATOR);
+			} else {
+				return TokenKind.ASSIGNMENT;
+			}
+			
+		case '.':
+			takeIt();
+			return (TokenKind.DOT);
+		case '(': 
+			takeIt();
+			return(TokenKind.LPAREN);
+
+		case ')':
+			takeIt();
+			return(TokenKind.RPAREN);
+			
+		case '{': 
+			takeIt();
+			return(TokenKind.LBRACKET);
+
+		case '}':
+			takeIt();
+			return(TokenKind.RBRACKET);
+		case '[' :
+			takeIt();
+			return(TokenKind.SQLBRACKET);
+		case ']':
+			takeIt();
+			return(TokenKind.SQRBRACKET);
+			
+		case ';':
+			takeIt();
+			return(TokenKind.SEMICOLON);
+		case ',':
+			takeIt();
+			return(TokenKind.COMMA);
+		
+		default:
+			scanError("Unrecognized character '" + currentChar + "' in input");
+			return(TokenKind.ERROR);
+		}
+		
 	}
 	
 	private Token makeToken( TokenType toktype ) {
